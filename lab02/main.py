8a9405d5768e87a209eab96119a29bffd529b031
SERGEYDJUM <39106181+SERGEYDJUM@users.noreply.github.com>
2024-11-12 12:27:49 +0300

    [lab02] Add comments and improve generation

--- lab02/main.py
+++ lab02/main.py
@@ -4,23 +4,25 @@ from tkinter import ttk
 from model_infer import Model
 
 
-class App:
+class App(Tk):
+    """Application UI"""
+
     base_font = ("CaskaydiaCove NF", 12)
 
     def __init__(self) -> None:
+        Tk.__init__(self)
         self.model: Model = None
         self.temperature = 1.0
         self.token_cnt = 256
 
-        self.window = Tk()
-        self.window.wm_resizable(False, False)
-        self.window.title("Генерация текста")
+        self.wm_resizable(False, False)
+        self.title("Генерация текста")
 
-        self.input = Text(self.window, height=8, font=self.base_font)
+        self.input = Text(height=8, font=self.base_font)
         self.input.grid(row=0, column=0)
 
         # Controls begin
-        self.btn_area = ttk.Frame(self.window)
+        self.btn_area = ttk.Frame()
         self.btn_area.grid(row=1, column=0)
 
         self.load_btn = ttk.Button(
@@ -32,7 +34,7 @@ class App:
             self.btn_area, text="Предсказать продолжение", command=self.predict
         )
         self.exec_btn.grid(row=0, column=1)
-        
+
         self.token_cnt_label = ttk.Label(self.btn_area, text="Температура: ")
         self.token_cnt_label.grid(row=0, column=2, padx=5)
 
@@ -43,56 +45,60 @@ class App:
             value=self.temperature,
             orient=HORIZONTAL,
         )
-        
+
         self.temp_slider.grid(row=0, column=3, padx=5)
 
         self.token_cnt_inp = ttk.Entry(self.btn_area, width=7)
         self.token_cnt_inp.insert(0, str(self.token_cnt))
         self.token_cnt_inp.grid(row=0, column=4)
-        
+
         self.token_cnt_label = ttk.Label(self.btn_area, text="токенов")
         self.token_cnt_label.grid(row=0, column=5)
         # Controls end
 
-        self.output = Text(self.window, font=self.base_font)
+        self.output = Text(font=self.base_font)
         self.output.grid(row=2, column=0)
 
     def load_file(self) -> None:
+        """Opens a dialogue to pick input text file"""
         path = filedialog.askopenfilename()
-        
+
         with open(path, encoding="utf-8") as file:
             text: str = file.read().strip()
+
+            # Clear input Text
             self.input.delete("1.0", "999.999")
             self.input.insert("1.0", text)
 
     def predict(self) -> None:
+        """Runs model using prompt from input Text and writes result into output Text"""
+
+        # Load model on demand to shorten UI startup
         if self.model is None:
             self.model = Model()
 
+        # Update generation parameters
         self.token_cnt = int(self.token_cnt_inp.get())
         self.temperature = float(self.temp_slider.get())
-                
+
         prompt = self.input.get("1.0", "999.999").strip()
-        
-        print(f"[{self.temperature:.2f} | {self.token_cnt}] {prompt}")
-        
+
+        print(f"[{self.temperature:.2f} | {self.token_cnt}] >>> {prompt}")
+
         if not prompt:
             return
 
         response = self.model.execute_prompt(
             prompt, temperature=self.temperature, max_new_tokens=self.token_cnt
         )
-        
+
         response = response.strip().removeprefix(prompt).strip()
-        
-        print(f">>> {response}")
+
+        print(f"<<< {response}")
 
         self.output.delete("1.0", "999.999")
         self.output.insert("1.0", response)
 
-    def run(self) -> None:
-        self.window.mainloop()
-
 
 if __name__ == "__main__":
-    App().run()
+    App().mainloop()
--- lab02/model_infer.py
+++ lab02/model_infer.py
@@ -2,23 +2,31 @@ from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 
 
 class Model:
+    """'ruGPT-3 Large' model executor"""
+
+    hf_repo = "ai-forever/rugpt3large_based_on_gpt2"
+
     def __init__(self) -> None:
-        self.tokenizer = GPT2TokenizerFast.from_pretrained(
-            "ai-forever/rugpt3large_based_on_gpt2"
-        )
+        self.tokenizer = GPT2TokenizerFast.from_pretrained(self.hf_repo)
         self.model = GPT2LMHeadModel.from_pretrained(
-            "ai-forever/rugpt3large_based_on_gpt2",
+            self.hf_repo,
+            # Following options assume modern Nvidia GPU
             torch_dtype="float16",
             device_map="cuda:0",
         )
 
     def execute_prompt(self, prompt: str, **kwargs) -> str:
+        """Generates tokens from prompt with pass-through keyword arguments"""
         tokens = self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
 
-        gen_tokens = self.model.generate(
+        beam_output = self.model.generate(
             tokens.input_ids,
+            # Avoids repeting n-grams of length 2
+            no_repeat_ngram_size=2,
+            # These two parameters enable Beam-search Multinomial Sampling
             do_sample=True,
+            num_beams=4,
             **kwargs,
         )
 
-        return self.tokenizer.batch_decode(gen_tokens)[0]
+        return self.tokenizer.decode(beam_output[0], skip_special_tokens=True)
