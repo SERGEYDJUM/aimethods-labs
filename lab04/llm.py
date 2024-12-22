a02ca4029c19c90445ad320e1d10a3c9d0e1de70
SERGEYDJUM <39106181+SERGEYDJUM@users.noreply.github.com>
2024-12-22 16:52:39 +0300

    lab04: Implement llm component

--- lab04/llm.py
+++ lab04/llm.py
@@ -1,63 +1,98 @@
 import os, asyncio
-from loguru import logger
-from uuid import UUID, uuid4
 from exllamav2.generator import ExLlamaV2DynamicGeneratorAsync, ExLlamaV2DynamicJobAsync
 from exllamav2 import (
     ExLlamaV2,
     ExLlamaV2Config,
-    ExLlamaV2Cache,
     ExLlamaV2Tokenizer,
     ExLlamaV2Cache,
 )
 
+from prompt import Phi3PromptFormat
 
-class AsyncLLM:
-    def __init__(self, model_dir: str = os.environ["EXL_MODEL_DIR"]):
+
+class AsyncPhi3:
+    """Async inference for Phi-3 family models."""
+
+    def __init__(self, model_dir: str = os.environ["EXL_MODEL_DIR"]) -> None:
+        """Loads a model from Phi-3 family and it's tokenizer.
+
+        Args:
+            model_dir (str, optional): Directory containing weights and config. Defaults to os.environ["EXL_MODEL_DIR"].
+        """
         config = ExLlamaV2Config(model_dir)
-        config.arch_compat_overrides()
-
         model = ExLlamaV2(config)
-        cache = ExLlamaV2Cache(model, lazy=True, max_seq_len=2048)
+        cache = ExLlamaV2Cache(model, max_seq_len=2048)
         model.load_autosplit(cache, progress=True)
 
         self.tokenizer = ExLlamaV2Tokenizer(config)
+        self.prompt_format = Phi3PromptFormat()
+        self.stop_conditions = list(
+            filter(
+                lambda x: x is not None,
+                self.prompt_format.stop_conditions_ids(self.tokenizer),
+            )
+        )
 
         self.generator = ExLlamaV2DynamicGeneratorAsync(
             model=model,
             cache=cache,
             tokenizer=self.tokenizer,
-            paged=False, #Disable Linux-only Flash Attention 2, removes job batch processing
+            paged=False,  # Disable Linux-only Flash Attention 2, results in no batching
         )
 
-    async def complete_text(self, prompt: str, job_id: UUID):
-        # Create an iterable, async job. The job will be transparently batched 
+    async def complete_text(
+        self,
+        user_prompt: str,
+        system_prompt: str | None = None,
+        max_new_tokens: int = 64,
+        **kwargs,
+    ) -> str:
+        """Anwer the prompt asynchronously.
+
+        Args:
+            user_prompt (str): User's input text.
+            system_prompt (str | None, optional): Instructions for LLM. Defaults to None.
+            max_new_tokens (int, optional): Maximum length of the output. Defaults to 64.
+        """
+
+        # Create an iterable, async job. The job will be transparently batched
         # together with other jobs on the generator if possible.
+        prompt = self.prompt_format.format_prompt(
+            user_prompt=user_prompt, system_prompt=system_prompt
+        )
+
         job = ExLlamaV2DynamicJobAsync(
             self.generator,
-            input_ids=self.tokenizer.encode(prompt, add_bos=False),
-            max_new_tokens=100,
+            input_ids=self.prompt_format.encode_prompt(self.tokenizer, prompt),
+            max_new_tokens=max_new_tokens,
+            stop_conditions=self.stop_conditions,
+            **kwargs,
         )
 
-        text = prompt
-
-        async for new_text in job:
-            text += new_text.get("text", "")
-            
-        logger.debug(f"llm: completed job {job_id}")
-
-        return text
+        return "".join([frag.get("text", "") async for frag in job])
 
 
 if __name__ == "__main__":
+
     async def test():
-        llm = AsyncLLM()
+        llm = AsyncPhi3()
 
         prompts = [
-            "Once upon a time, there was",
-            "asyncio in Python is a great feature because",
+            "Меня зовут Сергей",
+            "Николай дома? Я Альберт, его друг.",
         ]
 
-        tasks = [llm.complete_text(prompt, uuid4()) for prompt in prompts]
+        tasks = [
+            llm.complete_text(
+                prompt,
+                system_prompt=(
+                    "You are tasked with extracting a name from the input text. "
+                    "If text contains more than one name, pick one that is most likely user's. "
+                    'You must end your response after that with "<|end|>".'
+                ),
+            )
+            for prompt in prompts
+        ]
         outputs = await asyncio.gather(*tasks)
 
         print()
---
+++ lab04/prompt.py
@@ -0,0 +1,73 @@
+from exllamav2 import ExLlamaV2Tokenizer
+from torch import Tensor
+
+
+class Phi3PromptFormat:
+    botname = "Chatbot"
+    username = "User"
+    description = "Phi3-instruct models"
+
+    def __init__(self):
+        pass
+
+    def default_system_prompt(self) -> str:
+        return "You are a helpful AI assistant."
+
+    def first_prompt(self, sysprompt: str | None = None) -> str:
+        r = """<s>"""
+        if sysprompt:
+            r += """<|system|>\n""" + """<|system_prompt|>""" + """<|end|>\n"""
+        r += """<|user|>\n""" + """<|user_prompt|><|end|>\n""" + """<|assistant|>\n"""
+        return r
+
+    def subs_prompt(self) -> str:
+        return (
+            """<|end|>\n"""
+            + """<|user|>\n"""
+            + """<|user_prompt|>"""
+            + """<|end|>\n"""
+            + """<|assistant|>\n"""
+        )
+
+    def stop_conditions_strings(self) -> list[str]:
+        return ["<|end|>", "<|assistant|>", "<|endoftext|>"]
+
+    def stop_conditions_ids(self, tokenizer: ExLlamaV2Tokenizer) -> list[int]:
+        stops = [tokenizer.eos_token_id]
+
+        stops.extend(
+            filter(
+                lambda x: x is not None,
+                map(lambda x: tokenizer.single_id(x), self.stop_conditions_strings()),
+            )
+        )
+
+    def encoding_options(self) -> tuple[bool, bool, bool]:
+        return False, False, True
+
+    def print_extra_newline(self) -> bool:
+        return True
+
+    def format_prompt(
+        self, user_prompt: str, system_prompt: str | None = None, first: bool = True
+    ) -> str:
+        if not system_prompt:
+            system_prompt = self.default_system_prompt()
+
+        if first:
+            return (
+                self.first_prompt(system_prompt)
+                .replace("<|system_prompt|>", system_prompt)
+                .replace("<|user_prompt|>", user_prompt)
+            )
+        else:
+            return self.subs_prompt().replace("<|user_prompt|>", user_prompt)
+
+    def encode_prompt(self, tokenizer: ExLlamaV2Tokenizer, text: str) -> Tensor:
+        add_bos, add_eos, encode_special_tokens = self.encoding_options()
+        return tokenizer.encode(
+            text,
+            add_bos=add_bos,
+            add_eos=add_eos,
+            encode_special_tokens=encode_special_tokens,
+        )
